ec5e33158c3ee929d8f94a49bc4b853f211f9fee
ThakuraAnki <codeankit.thakur@gmail.com>
2026-04-13 13:03:10 +0530

    Initial commit

---
+++ 3889.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define FOR(n)for(int i = 0; i < n; i++)
+#define ll long long
+using namespace std;
+
+
+int main(){
+        string s;
+        cin>>s;
+        int ans=0;
+        map<char,int>m;
+        for(auto a : s){
+            m[a]++;
+        }
+        for(char val='a';val<='z';val++){
+            int apsdiff=abs(m[val]-m[('z'-(val-'a'))]);
+            ans+=apsdiff;
+            cout<<ans<<" ";
+        }
+        for(char i='0';i<='9';i++){
+            int apsdiff=abs(m[i]-m[('9'-(i-'0'))]);
+            ans+=apsdiff;
+            cout<<ans<<" ";
+
+        }
+        
+}
